4e9e2855aba48a1ec7e1875c15ea14b078cf0c53
Tsuyoshi Umeno <umeno_tsuyoshi@tufs.ac.jp>
2018-05-11 15:20:45 +0900

    言語選択の認識する言語を追加して実験。ミャンマーは全く動作せず。

--- mod_form.php
+++ mod_form.php
@@ -66,29 +66,57 @@ class mod_puhrec_mod_form extends moodleform_mod {
             $this->add_intro_editor();
         }
         //-------------------------------------------------------------------------------
-		// ここから独自の実装
+        // ここから独自の実装
         // Adding the rest of puhrec settings, spreading all them into this fieldset
         // ... or adding more fieldsets ('header' elements) if needed for better logic.
-		// $mform->addElement('static', 'label1', 'puhrecsetting1', 'Your puhrec fields go here. Replace me!')
+        // $mform->addElement('static', 'label1', 'puhrecsetting1', 'Your puhrec fields go here. Replace me!')
         //-------------------------------------------------------------------------------
         // 練習用テキスト
         $mform->addElement('textarea', 'practicetext', get_string('practicetext', 'puhrec'), array('size' => '64'));
         if (!empty($CFG->formatstringstriptags)) {
-        	$mform->setType('practicetext', PARAM_TEXT);
+            $mform->setType('practicetext', PARAM_TEXT);
         } else {
-        	$mform->setType('practicetext', PARAM_CLEAN);
+            $mform->setType('practicetext', PARAM_CLEAN);
         }
         $mform->addRule('practicetext', null, 'required', null, 'client');
         $mform->addRule('practicetext', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
         $mform->addHelpButton('practicetext', 'puhrecname', 'puhrec');
         // 練習用テキストの言語
-        $options = array("ja-JP"=>"ja-JP","en-US"=>"en-US","zh-CN"=>"zh-CN",);
+        $options = array(
+            "ja-JP"=>"日本語",
+            "de"=>"ドイツ語",
+            "pl"=>"ポーランド語",
+            "cs"=>"チェコ語",
+            "fr-FR"=>"フランス語",
+            "it"=>"イタリア語",
+            "en-US"=>"英語",
+            "es-ES"=>"スペイン語",
+            "pt-pt"=>"ポルトガル語",
+            "ru-RU"=>"ロシア語",
+            "mn"=>"モンゴル語",
+            "zh-CN"=>"中国語",
+            "ko-KR"=>"韓国語",
+            "id"=>"インドネシア語",
+            "ms"=>"マレーシア語",
+            "tl"=>"フィリピン語",
+            "th-TH"=>"タイ語",
+            "lo-LA"=>"ラオス語",
+            "vi"=>"ベトナム語",
+            "km"=>"カンボジア語",
+            "my"=>"ビルマ語",
+            "ur"=>"ウルドゥー語",
+            "hi"=>"ヒンディー語",
+            "bn"=>"ベンガル語",
+            "ar"=>"アラビア語",
+            "fa"=>"ペルシャ語",
+            "tr-TR"=>"トルコ語",
+        );
         $name = get_string('lang', 'puhrec');
         $mform->addElement('select', 'lang', $name, $options);
         $mform->addHelpButton('lang', 'lang', 'puhrec');
-        
-        
-        
+
+
+
         //開始日時、終了日時
         $mform->addElement('date_time_selector', 'timeavailable', get_string('availabledate', 'puhrec'), array('optional'=>true));
         $mform->setDefault('timeavailable', time());
@@ -100,16 +128,16 @@ class mod_puhrec_mod_form extends moodleform_mod {
         $mform->setType('maxduration', PARAM_INT);
         $mform->addHelpButton('maxduration', 'maxduration', 'puhrec');
         $mform->setDefault('maxduration', get_config('puhrec' , 'maxduration'));
-        
+
         // 音ファイルの数の上限
         $options = array();
         for ($i = 1; $i <= get_config('puhrec' , 'maxnumber'); $i++) {
-        	$options[$i] = $i;
+            $options[$i] = $i;
         }
         $name = get_string('maxnumber', 'puhrec');
         $mform->addElement('select', 'maxnumber', $name, $options);
         $mform->addHelpButton('maxnumber', 'maxnumber', 'puhrec');
-        
+
         //提出日以降の送信を阻止する
         $mform->addElement('selectyesno', 'preventlate', get_string('preventlate', 'puhrec'));
 
@@ -117,7 +145,7 @@ class mod_puhrec_mod_form extends moodleform_mod {
 //        $mform->addElement('static', 'label2', 'puhrecsetting2', 'Your puhrec fields go here. Replace me!');
 
         //-------------------------------------------------------------------------------
-		// 以下テンプレのまま
+        // 以下テンプレのまま
         //-------------------------------------------------------------------------------
         // Add standard grading elements.
         $this->standard_grading_coursemodule_elements();
@@ -129,16 +157,16 @@ class mod_puhrec_mod_form extends moodleform_mod {
         $this->add_action_buttons();
     }
     function validation($data, $files) {
-    	$errors = parent::validation($data, $files);
-    	$system_maxduration = 1200; // in case of admin failuer.
-    	$site_maxduration = get_config('puhrec' , 'maxduration');
-    	$maxduration = ($system_maxduration>$site_maxduration)?$site_maxduration:$system_maxduration;
-    	if(0<= $data['maxduration'] && $data['maxduration'] <= $maxduration){
-    		return $errors;
-    	}else{
-    		$error['maxduration'] = get_string('maxdurationerror', 'puhrec',$site_maxduration);
-    		return $error;
-    	}
+        $errors = parent::validation($data, $files);
+        $system_maxduration = 1200; // in case of admin failuer.
+        $site_maxduration = get_config('puhrec' , 'maxduration');
+        $maxduration = ($system_maxduration>$site_maxduration)?$site_maxduration:$system_maxduration;
+        if(0<= $data['maxduration'] && $data['maxduration'] <= $maxduration){
+            return $errors;
+        }else{
+            $error['maxduration'] = get_string('maxdurationerror', 'puhrec',$site_maxduration);
+            return $error;
+        }
     }
-    
+
 }
